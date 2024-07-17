2f6751f8f48b3360f4626e7649c13bc0e9d75c42
marimed5 <marmed1108@gmail.com>
2024-07-17 23:26:46 +0400

    Update HomePage.jsx

--- react-project-optimusfox-master/src/components/user/User.jsx
+++ react-project-optimusfox-master/src/components/user/User.jsx
@@ -1,3 +1,11 @@
-export function User() {
-  return <div>User</div>;
+export function User({user}) {
+  const {id, name, role, age} = user;
+
+  return (
+    <div>
+      <h1>{name}</h1>
+      <h2>Role as {role}</h2>
+      <h2>{age} years old</h2>
+    </div>
+  );
 }
